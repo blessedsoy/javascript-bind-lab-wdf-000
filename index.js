72d79042ee1775b1a784c1dfb017bcbaa1566cc6
blessedsoy <unmigong@gmail.com>
2016-11-12 23:08:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function(){
+    setInterval(this.time += 1, 1000)
+  }
+}
+
+function censor(word, string) {
+  const matchWord = new RegExp(word, 'gi')
+  return string.replace(matchWord, 'BLEEP');
+}
+
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');
